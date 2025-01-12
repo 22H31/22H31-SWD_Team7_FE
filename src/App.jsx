a74a5dd09ffc76e56b69dd7dbd5ad26c8e9ed248
KS_REACT_SE170316 <quangdmse170316@fpt.edu.vn>
2025-01-12 15:41:23 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { Route, Routes } from "react-router"
+import Home from "./page/Home/Home"
+
+
+
+function App() {
+ 
+
+  return (
+    <>
+     <Routes>
+      <Route path="/" element={< Home/>} />
+    </Routes>
+    </>
+  )
+}
+
+export default App
