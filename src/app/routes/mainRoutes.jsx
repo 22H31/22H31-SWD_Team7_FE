e816fecf418ad23c499e9eec44f4e5d43841ba1c
QuocHiep <130179403+QuocHiep31003@users.noreply.github.com>
2025-03-11 14:14:08 +0700

    api-blogblog

--- src/app/routes/mainRoutes.jsx
+++ src/app/routes/mainRoutes.jsx
@@ -1,33 +1,33 @@
 // import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "../../app/pages/Home/Home";
-import Sale from "../../app/pages/Sale/Sale";
-import Profile from "../../app/pages/Profile/Profile";
-import Login from "../../app/pages/Login/Login";
-import Register from "../../app/pages/SignUp/Register";
-import ForgotPassword from "../../app/pages/ForgotPassword/ForgotPassword";
+import { useLocation } from "react-router";
+import { Route, Routes } from "react-router-dom";
 import PageNotFound from "../../app/layouts/PageNotFound/PageNotFound";
 import Blog from "../../app/pages/Blog/Blog";
-import BlogDetail from "../pages/BlogDetail";
-import Cart from "../pages/cart";
-import CustomerSupport from "../pages/CustomerSupport";
-import FooterComponent from "../layouts/Footer/Footer";
-import { useLocation } from "react-router";
-import Checkout from "../pages/Checkout";
+import ForgotPassword from "../../app/pages/ForgotPassword/ForgotPassword";
+import Home from "../../app/pages/Home/Home";
+import Login from "../../app/pages/Login/Login";
+import Profile from "../../app/pages/Profile/Profile";
+import Sale from "../../app/pages/Sale/Sale";
+import Register from "../../app/pages/SignUp/Register";
 import GeminiChat from "../AI/GeminiChat";
 import AdminLayout from "../layouts/adminLayout/adminLayout";
-import ProductsGrid from "../pages/Admin/productManage/productsGrid";
-import ProductStock from "../pages/Admin/productStock/productStock";
-import TeamManage from "../pages/Admin/teamManage/teamPage";
+import FooterComponent from "../layouts/Footer/Footer";
+import BlogManage from "../pages/Admin/blogManage/blogPage";
 import Brand from "../pages/Admin/brandManage/brandPage";
 import Category from "../pages/Admin/categoryManage/categoryPage";
 import CategoryTitle from "../pages/Admin/categoryManage/categoryTitlePage";
-import BlogManage from "../pages/Admin/blogManage/blogPage";
+import ProductsGrid from "../pages/Admin/productManage/productsGrid";
+import ProductStock from "../pages/Admin/productStock/productStock";
+import TeamManage from "../pages/Admin/teamManage/teamPage";
+import BlogDetail from "../pages/BlogDetail";
+import Cart from "../pages/cart";
+import Checkout from "../pages/Checkout";
+import CustomerSupport from "../pages/CustomerSupport";
 import PaymentSuccess from "../pages/PaymentSuccess";
 
 export default function MainRoutes() {
   const location = useLocation();
-  const hideFooter = ["/login", "/forgotPassword", "/register"].some((path) =>
+  const hideFooter = ["/login", "/forgotPassword", "/register","/admin"].some((path) =>
     location.pathname.includes(path)
   );
   return (
@@ -40,20 +40,20 @@ export default function MainRoutes() {
         <Route path="/register" element={<Register />} />
         <Route path="/forgotPassword" element={<ForgotPassword />} />
         <Route path="/blog" element={<Blog />} />
-        <Route path="/blogDetail" element={<BlogDetail />} />
+        <Route path="/blog/:blogId" element={<BlogDetail />} /> 
         <Route path="/cart" element={<Cart />} />
         <Route path="/customerSupport" element={<CustomerSupport />} />
         <Route path="/checkout" element={<Checkout />} />
         <Route path="/paymentSuccess" element={<PaymentSuccess/>} />
 
         <Route path="/admin" element={<AdminLayout />}>
-            <Route path="products" element={<ProductsGrid />} />
-            <Route path="productStock" element={<ProductStock />} />
-            <Route path="teamPage" element={<TeamManage />} />
-            <Route path="brandPage" element={<Brand />} />
-            <Route path="categoryPage" element={<Category />} />
-            <Route path="categoryTitlePage" element={<CategoryTitle />} />
-            <Route path="blog" element={<BlogManage />} />
+        <Route path="products" element={<ProductsGrid />} />
+        <Route path="productStock" element={<ProductStock />} />
+        <Route path="teamPage" element={<TeamManage />} />
+        <Route path="brandPage" element={<Brand />} />
+        <Route path="categoryPage" element={<Category />} />
+        <Route path="categoryTitlePage" element={<CategoryTitle />} />
+        <Route path="blog" element={<BlogManage />} />
         </Route>
 
         {/* Uncomment to handle unknown routes */}
